c95b77398b3d61645d30bb066811e99ffc90091d
siddharth-mavani <siddharthmavani555@gmail.com>
2023-08-17 15:35:56 +0530

    fixed preview, removed footer

--- src/views/DataPreprocessing.js
+++ src/views/DataPreprocessing.js
@@ -1,14 +1,14 @@
-// import { Row, Col, Card, CardBody, Button } from "reactstrap";
-// import "../assets/css/paper-dashboard.css"
+import { Row, Col, Card, CardBody, Button } from "reactstrap";
+import "../assets/css/paper-dashboard.css"
 
-// function DataPreprocessing() {
-//     return (
-//         <>
-//             <div className="content">
-//                 Hello BH7
-//             </div>
-//         </>
-//     )
-// }
+function DataPreprocessing() {
+    return (
+        <>
+            <div className="content">
+                Hello BH7
+            </div>
+        </>
+    )
+}
 
-// export default DataPreprocessing;
+export default DataPreprocessing;
--- src/views/Preview.js
+++ src/views/Preview.js
@@ -1,52 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Table } from "reactstrap";
 import Papa from "papaparse";
 
 function CSVDisplay() {
   const [parsedCsvData, setParsedCsvData] = useState([]);
 
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        const text = e.target.result;
-        Papa.parse(text, {
-          complete: (result) => {
-            const data = result.data.slice(1, 51); // Slice to get first 50 rows
-            setParsedCsvData(data);
-          },
-        });
-      };
-
-      reader.readAsText(file);
-    }
-  };
+  useEffect(() => {
+    fetch('./archive/adult.csv')
+      .then(response => response.text())
+      .then(text => {
+        const parsedData = Papa.parse(text, { header: true });
+        setParsedCsvData(parsedData.data.slice(0,5));
+        console.log('text', parsedCsvData);
+      });
+  }, []);
 
   return (
     <div className="content">
-      <input type="file" accept=".csv" onChange={handleFileUpload} />
-      <Table>
-        <thead>
-          {parsedCsvData.length > 0 && (
-            <tr>
-              {Object.keys(parsedCsvData[0]).map((header, index) => (
-                <th key={index}>{header}</th>
-              ))}
-            </tr>
-          )}
-        </thead>
-        <tbody>
-          {parsedCsvData.map((row, rowIndex) => (
-            <tr key={rowIndex}>
-              {Object.values(row).map((cellValue, cellIndex) => (
-                <td key={cellIndex}>{cellValue}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <div style={{ overflowX: 'auto' }}>
+        <Table>
+          <thead>
+            {parsedCsvData.length > 0 && (
+              <tr>
+                {Object.keys(parsedCsvData[0]).map((header, index) => (
+                  <th key={index}>{header}</th>
+                ))}
+              </tr>
+            )}
+          </thead>
+          <tbody>
+            {parsedCsvData.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {Object.values(row).map((cellValue, cellIndex) => (
+                  <td key={cellIndex}>{cellValue}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
