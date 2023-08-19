6e4f781f348712a1e8f9795bac66c08c96e51c7f
Ayush Agrawal <ayush.agriiit@gmail.com>
2023-08-19 22:55:52 +0530

    training page

--- src/views/train.js
+++ src/views/train.js
@@ -14,13 +14,20 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
+import { LinearProgress } from "@mui/material";
 
-import { Col, Row } from "reactstrap";
+import { Col, Row, Button as ReactStrapButton } from "reactstrap";
 
 function Train() {
     const [activeStep, setActiveStep] = React.useState(0);
-    const steps = ['Model Details', 'Training Options', 'Train the model'];
+    const [modelType, setModelType] = React.useState('');
+    const [metricType, setMetricType] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+
+    const steps = ['Training Options', 'Model Selection', 'Train the model'];
     const democolumns = ['Age', 'Amount', 'Income', 'Fraud Type']
+    const classifiers = ['Logistic Regression', 'Decision Tree', 'Random Forest', 'AdaBoost', 'Naive Bayes', 'KNN', 'SVM']
+    const metrics = ['Accuracy', 'Precision', 'Recall', 'F1 Score', 'AUC']
 
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -33,6 +40,33 @@ function Train() {
     const handleReset = () => {
         setActiveStep(0);
     };
+
+    const handleModelTypeChange = (event) => {
+        console.log(event.target.value);
+        setModelType(event.target.value);
+    }
+
+    const handleMetricTypeChange = (event) => {
+        console.log(event.target.value);
+        setMetricType(event.target.value);
+    }
+
+    const handleTraining = async (event) => {
+        event.preventDefault();
+        try {
+            setLoading(true); // Start loading
+
+            // Simulate a 5-second delay
+            await new Promise(resolve => setTimeout(resolve, 5000));
+
+            // setApiResponse('Simulated API response'); // Update the API response
+        } catch (error) {
+            console.error('API error:', error);
+        } finally {
+            setLoading(false); // Stop loading
+        }
+    }
+
     return (
         <div className="content">
             <Row>
@@ -60,9 +94,9 @@ function Train() {
                         </React.Fragment>
                     ) : (
                         <React.Fragment>
-                           
+
                             {activeStep === 0 ?
-                                <div style={{marginTop: "1.5rem"}}>
+                                <div style={{ marginTop: "1.5rem" }}>
                                     <Row className="align-items-center mb-3">
                                         <Col md="2">
                                             Select Dataset:
@@ -134,48 +168,163 @@ function Train() {
                                         </Col>
                                     </Row>
 
-                                    <Row className="align-items-center mb-3">
-                                        <Col md="2">
-                                            Select Model Type:
-                                        </Col>
-                                        <Col md="6">
-                                            <FormControl>
-                                                <RadioGroup
-                                                    aria-labelledby="demo-controlled-radio-buttons-group"
-                                                // name="controlled-radio-buttons-group"
-                                                // value={value}
-                                                // onChange={handleChange}
-                                                >
-                                                    <FormControlLabel
-                                                        value="AutoML"
-                                                        control={<Radio />}
-                                                        label={
-                                                            <div style={{marginTop: '0.8rem'}}>
-                                                                AutoML
-                                                                <div style={{ fontSize: '12px', color: 'gray'}}>
-                                                                    Train a classifier automatically which maximizes the accuracy. The model is trained using various algorithms and the best one is selected.
-                                                                </div>
-                                                            </div>
-                                                        }
-                                                    />
-                                                    <FormControlLabel
-                                                        value="AutoML"
-                                                        control={<Radio />}
-                                                        label={
-                                                            <div style={{marginTop: '0.8rem'}}>
-                                                                Select a model type
-                                                                <div style={{ fontSize: '12px', color: 'gray'}}>
-                                                                    Select a particular model type and train the model.
-                                                                </div>
-                                                            </div>
-                                                        }
-                                                    />
-                                                </RadioGroup>
-                                            </FormControl>
-                                        </Col>
-                                    </Row>
+
 
                                 </div> : null}
+                            {activeStep == 1 ? <div style={{ marginTop: "1.5rem" }}>
+                                <Row className="mb-3">
+                                    <Col md="2" style={{ marginTop: "1rem" }}>
+                                        Optimization Objective:
+                                    </Col>
+                                    <Col md="6">
+                                        <FormControl>
+                                            <RadioGroup
+                                                aria-labelledby="demo-controlled-radio-buttons-group"
+                                                // name="controlled-radio-buttons-group"
+                                                value={metricType}
+                                                onChange={handleMetricTypeChange}
+                                            >
+                                                <FormControlLabel
+                                                    value="AutoSelect"
+                                                    control={<Radio />}
+                                                    label={
+                                                        <div style={{ marginTop: '0.8rem' }}>
+                                                            Select Automatically based on dataset
+                                                            <div style={{ fontSize: '12px', color: 'gray' }}>
+                                                                The metric to be optimized is selected automatically based on the dataset.
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                />
+                                                <FormControlLabel
+                                                    value="CustomMetric"
+                                                    control={<Radio />}
+                                                    label={
+                                                        <div style={{ marginTop: '0.8rem' }}>
+                                                            Select a particular metric
+                                                            <div style={{ fontSize: '12px', color: 'gray' }}>
+                                                                Select a particular metric to be optimized.
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                />
+                                                {metricType == "CustomMetric" ? <div>
+                                                    <FormControl fullWidth>
+                                                        <InputLabel id="label">Select Metric</InputLabel>
+                                                        <Select
+                                                            labelId="label"
+                                                            label="Select Classifier"
+                                                            fullWidth
+                                                        // onChange={handleChange}
+                                                        >
+                                                            {metrics.map((column) => (
+                                                                <MenuItem value={column}>{column}</MenuItem>
+                                                            ))}
+                                                        </Select>
+                                                    </FormControl>
+                                                </div> : null}
+                                            </RadioGroup>
+                                        </FormControl>
+                                    </Col>
+                                </Row>
+                                <Row className=" mb-3">
+                                    <Col md="2" className="mt-3">
+                                        Select Model Type:
+                                    </Col>
+                                    <Col>
+                                        <FormControl>
+                                            <RadioGroup
+                                                aria-labelledby="demo-controlled-radio-buttons-group"
+                                                // name="controlled-radio-buttons-group"
+                                                row
+                                                value={modelType}
+                                                onChange={handleModelTypeChange}
+                                            >
+                                                <FormControlLabel
+                                                    value="AutoML"
+                                                    control={<Radio />}
+                                                    label={
+                                                        <div style={{ marginTop: '0.8rem' }}>
+                                                            AutoML
+                                                            <div style={{ fontSize: '12px', color: 'gray', maxWidth: '50%' }}>
+                                                                Train a classifier automatically which maximizes the selected metric. The model is trained using various algorithms and the best one is selected.
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                />
+                                                <FormControlLabel
+                                                    value="CustomModel"
+                                                    control={<Radio />}
+                                                    label={
+                                                        <div style={{ marginTop: '0.8rem' }}>
+                                                            Select a model type
+                                                            <div style={{ fontSize: '12px', color: 'gray' }}>
+                                                                Select a particular model type and train the model.
+                                                            </div>
+                                                        </div>
+                                                    }
+                                                />
+                                                {modelType == "CustomModel" ? <div>
+                                                    <FormControl fullWidth>
+                                                        <InputLabel id="label">Select Classifier</InputLabel>
+                                                        <Select
+                                                            labelId="label"
+                                                            label="Select Classifier"
+                                                            fullWidth
+                                                        // onChange={handleChange}
+                                                        >
+                                                            {classifiers.map((column) => (
+                                                                <MenuItem value={column}>{column}</MenuItem>
+                                                            ))}
+                                                        </Select>
+                                                    </FormControl>
+                                                </div> : null}
+                                            </RadioGroup>
+                                        </FormControl>
+                                    </Col>
+                                </Row>
+                            </div> : null}
+                            {activeStep == 2 ? <div style={{ marginTop: "1.5rem" }}>
+                                <Row className="mb-3">
+                                    <Col>
+                                        <Typography
+                                            variant="h5"
+                                        >
+                                            Model Details
+                                        </Typography>
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col md="2">
+                                        Dataset:
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col md="2">
+                                        Model Name:
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col md="2">
+                                        Model Type:
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col md="2">
+                                        Optimization Objective:
+                                    </Col>
+                                </Row>
+                                <Row className="mb-3">
+                                    <Col>
+                                        <ReactStrapButton disabled={loading} onClick={handleTraining} >Start Training</ReactStrapButton>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        {loading ? <LinearProgress /> : null}
+                                    </Col>
+                                </Row>
+                            </div> : null}
                             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                                 <Button
                                     color="inherit"
